91b387fbd3c8606a47687a591506faf6ce073055
zky111111 <2537510863@qq.com>
2024-11-28 20:56:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-document.body.textContent = "Hello! 赵可怡 20220441232 22数据02班.";
+const h1 = document.createElement("h1");
+h1.textContent = "Hello! 赵可怡 20220441232 22数据02班.";
+document.body.appendChild(h1);
