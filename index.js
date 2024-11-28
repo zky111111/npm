13aea3938646b0774e99cde0663a257881ba94c4
zky111111 <2537510863@qq.com>
2024-11-28 20:48:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-import { formatDistanceToNow } from "date-fns";
-
-const date = "1996-09-13 10:00:00";
-const dateObj = new Date(date); // 将字符串转换为Date对象
-const timeAgo = formatDistanceToNow(dateObj); // 计算从现在到指定日期的时间差
-
-document.body.textContent = `Hello! My name is zky! Happy life. It's been ${timeAgo} since my birth.`;
+document.body.textContent = "Hello! My name is zky! Happy life.";
